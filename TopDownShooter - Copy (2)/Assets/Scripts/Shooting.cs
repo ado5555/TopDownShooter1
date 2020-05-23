9338d95634dbcab55be165385b34d0681bdd5f0f
ado5555 <61198587+ado5555@users.noreply.github.com>
2020-05-23 23:24:45 +0200

    asd
    sad

---
+++ TopDownShooter - Copy (2)/Assets/Scripts/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+
+
+    private GameManager m_GameManager;
+    public float m_Speed;
+    // Use this for initialization
+    void Start () {
+        m_GameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += Vector3.up * m_Speed * Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Cars"))
+        {
+            m_GameManager.m_score += 10;
+            Destroy(collision.gameObject);
+            this.gameObject.SetActive(false);
+
+        }
+    }
+}
---
+++ TopDownShooter - Copy (2)/Assets/Scripts/shield.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shield : MonoBehaviour {
+    private GameManager m_GameManager;
+    // Use this for initialization
+    void Start () {
+        m_GameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Cars"))
+        {
+            m_GameManager.m_score += 10;
+            Destroy(collision.gameObject);
+            this.gameObject.SetActive(false);
+
+        }
+    }
+}
+

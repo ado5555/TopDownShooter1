35af303ece5921bc16d5a7a7fdf7d58b4e122cb3
ado5555 <61198587+ado5555@users.noreply.github.com>
2020-05-23 18:06:28 +0200

    gtggggg

---
+++ TopDownShooter/Assets/Scripts/Cars.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cars : MonoBehaviour
+{
+
+
+    public float m_Speed;
+    private GameManager m_GameManager;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += Vector3.down * m_Speed * Time.deltaTime;
+    }
+    private void OnColliderEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ambulancia"))
+        {
+            Destroy(collision.collider.gameObject);
+            Destroy(this.gameObject);
+        }
+
+        if (collision.collider.CompareTag("Shield2"))
+        {
+            Destroy(collision.collider.gameObject);
+            Destroy(this.gameObject);
+        }
+
+    }
+}

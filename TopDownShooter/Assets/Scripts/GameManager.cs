68dd961323043f492af85a650a6d8819de7d83bb
ado5555 <61198587+ado5555@users.noreply.github.com>
2020-05-23 17:37:59 +0200

    Unity
    yes

---
+++ TopDownShooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public Text m_TextPuntuacio;
+    public Text m_TextVidas;
+    public GameObject[] m_AmbuVida;
+    public GameObject m_GameRestart;
+
+
+    [HideInInspector] public float m_score;
+    public int m_vidas;
+    public int m_misiles;
+    
+
+    void Start()
+    {
+ 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        m_TextPuntuacio.text = "Score: " + Mathf.Round(m_score);
+        m_TextVidas.text = "Vidas";
+        m_score += (Time.deltaTime);
+
+ 
+
+    }
+
+    public void RestartGame()
+    {
+        m_GameRestart.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ TopDownShooter/Assets/Scripts/Scrolling.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour {
+     public float m_Speed;
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += Vector3.down * m_Speed * Time.deltaTime;
+	}
+}

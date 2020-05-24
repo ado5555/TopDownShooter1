e64979a805ee804ec0167424e6f84de7082ce26b
ADO-program <60931560+ADO-program@users.noreply.github.com>
2020-05-24 20:35:47 +0200

    enemy
    enemy

---
+++ TopDownShooter/Assets/Scripts/ENEMY.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENEMY : MonoBehaviour
+{
+
+    public int m_speed = 2;
+    public Vector3 Vectordirector;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.Rotate(0, 180, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vectordirector * Time.deltaTime * m_speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.CompareTag("Limit1")))//toca con derecha
+        {
+            Vectordirector = Vector3.left;
+            this.transform.Rotate(0, 180, 0);
+
+        }
+        else if ((collision.CompareTag("Limit2")))//toca con izquierda
+        {
+            Vectordirector = Vector3.right;
+            this.transform.Rotate(0, 180, 0);
+
+        }
+       
+
+    }
+
+}
---
+++ TopDownShooter/Assets/Scripts/ENEMYTD.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENEMYTD : MonoBehaviour
+{
+
+
+    public int m_speed = 2;
+    public Vector3 Vectordirector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vectordirector * Time.deltaTime * m_speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.CompareTag("Limit1")))//toca con derecha
+        {
+            Vectordirector = Vector3.down;
+            this.transform.Rotate(0, 180, 0);
+
+        }
+        else if ((collision.CompareTag("Limit2")))//toca con izquierda
+        {
+            Vectordirector = Vector3.up;
+            this.transform.Rotate(0, 180, 0);
+
+        }
+        
+
+    }
+
+}
